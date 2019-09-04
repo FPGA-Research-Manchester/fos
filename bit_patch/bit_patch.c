a1b42eaeef780565070b40d5b500fc0fd4e8f48a
Joe Powell <joe2580.com@googlemail.com>
2019-09-04 10:47:30 +0000

    Fixed bit_patch headers

--- bit_patch/bit_patch.c
+++ bit_patch/bit_patch.c
@@ -1,6 +1,5 @@
 #include "bit_patch.h"
 
-#include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 
--- bit_patch/bit_patch.h
+++ bit_patch/bit_patch.h
@@ -1,4 +1,6 @@
 #pragma once
 
+#include <stdio.h>
+
 int relocate_bitstream_file(const char *infilename, const char *outfilename, int new_location);
 void relocate_bitstream(FILE* input, FILE* output, int slot);
